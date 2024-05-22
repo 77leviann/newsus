4d5bea0d36653304433c7cc608b61406e666d90d
77leviann <77leviann@gmail.com>
2024-05-22 08:57:05 +0700

    feat: bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:newsus/constants/name_routes_constant.dart';
 import 'package:newsus/screens/home/home_screen.dart';
 import 'package:newsus/screens/splash/splash_screen.dart';
+import 'package:newsus/widgets/custom_bottom_navigation_bar_widget.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -16,47 +17,48 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-        theme: FlexThemeData.light(
-          scheme: FlexScheme.blueM3,
-          surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-          blendLevel: 7,
-          subThemesData: const FlexSubThemesData(
-            blendOnLevel: 10,
-            blendOnColors: false,
-            useTextTheme: true,
-            useM2StyleDividerInM3: true,
-            alignedDropdown: true,
-            useInputDecoratorThemeInDialogs: true,
-          ),
-          visualDensity: FlexColorScheme.comfortablePlatformDensity,
-          useMaterial3: true,
-          swapLegacyOnMaterial3: true,
-          fontFamily: GoogleFonts.poppins().fontFamily,
+    return MaterialApp(
+      theme: FlexThemeData.light(
+        scheme: FlexScheme.blueM3,
+        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+        blendLevel: 7,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 10,
+          blendOnColors: false,
+          useTextTheme: true,
+          useM2StyleDividerInM3: true,
+          alignedDropdown: true,
+          useInputDecoratorThemeInDialogs: true,
         ),
-        darkTheme: FlexThemeData.dark(
-          scheme: FlexScheme.blueM3,
-          surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-          blendLevel: 13,
-          subThemesData: const FlexSubThemesData(
-            blendOnLevel: 20,
-            useTextTheme: true,
-            useM2StyleDividerInM3: true,
-            alignedDropdown: true,
-            useInputDecoratorThemeInDialogs: true,
-          ),
-          visualDensity: FlexColorScheme.comfortablePlatformDensity,
-          useMaterial3: true,
-          swapLegacyOnMaterial3: true,
-          fontFamily: GoogleFonts.poppins().fontFamily,
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        useMaterial3: true,
+        swapLegacyOnMaterial3: true,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
+      darkTheme: FlexThemeData.dark(
+        scheme: FlexScheme.blueM3,
+        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+        blendLevel: 13,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 20,
+          useTextTheme: true,
+          useM2StyleDividerInM3: true,
+          alignedDropdown: true,
+          useInputDecoratorThemeInDialogs: true,
         ),
-        themeMode: ThemeMode.system,
-        debugShowCheckedModeBanner: false,
-        initialRoute: NameRoutes.initRoute,
-        routes: {
-          NameRoutes.initRoute: (context) => const SplashScreen(),
-          NameRoutes.homeScreen: (context) => const HomeScreen(),
-        },
-      );
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        useMaterial3: true,
+        swapLegacyOnMaterial3: true,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      initialRoute: NameRoutes.initRoute,
+      routes: {
+        NameRoutes.initRoute: (context) => const SplashScreen(),
+        NameRoutes.homeScreen: (context) =>
+            const CustomBottomNavigationBarWidget(),
+      },
+    );
   }
 }
